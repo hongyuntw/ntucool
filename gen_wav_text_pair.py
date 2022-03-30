4d3f36ca9886f09b05ce85f3bfcf185a3cd58e09
hongyuntw <harrychiang0@gmail.com>
2022-03-30 22:49:57 +0800

    feat split_wav_script

--- gen_wav_text_pair.py
+++ gen_wav_text_pair.py
@@ -38,60 +38,49 @@ def get_transcript(video_id, transcript_language):
     return texts
 
 
-
-def create_wav_text_pairs(output_name, results):
-    wav_id = 0
+def split_wav_script(scripts, output_name):
     output_name = os.path.join('./outputs', output_name)
     os.makedirs(output_name, exist_ok=True)
     wav_name = f"{output_name}.wav"
     print(wav_name)
     audio = AudioSegment.from_file(wav_name)
+    cur_audio = AudioSegment.empty()
+    cur_sec = 0.0
+    cur_script = ''
+    wav_id = 0
+    single_wav_sec = 8.0
     print(len(audio))
 
-    for result in results:
-        file_name = str(wav_id).zfill(4)
-        
-        text, t1, t2 = result
-        t1 *= 1000
-        t2 *= 1000
-        new_audio = audio[t1:t2]
-        new_audio.export(f'{output_name}/{file_name}.wav', format="wav")
-        fp = open(f'{output_name}/{file_name}.txt', 'w', encoding='utf-8')
-        print(text, file=fp)
-        fp.close()
-        wav_id += 1
-
-
-
-def split_script(scripts):
-    cur_str = ''
-    wav_start = 0.0
-    wav_end = 0.0
-    reset = True
-    single_wav_sec = 8.0
-    results = []
     for i, script in enumerate(scripts):
         text = script['text']
         start = float(script['start'])
         duration = float(script['duration'])
         end = start + duration
+
+        start *= 1000
+        end *= 1000
+        cur_audio += audio[start:end]
+        cur_script += ' ' + text
+        cur_sec += duration
+
+        if cur_sec >= single_wav_sec:
+            file_name = str(wav_id).zfill(4)
+            cur_audio.export(f'{output_name}/{file_name}.wav', format="wav")
+            fp = open(f'{output_name}/{file_name}.txt', 'w', encoding='utf-8')
+            print(cur_script, file=fp)
+            fp.close()
+            wav_id += 1
+            print(cur_script)
+
+            cur_audio = AudioSegment.empty()
+            cur_sec = 0.0
+            cur_script = ''
         
-        if reset:
-            wav_start = start
-            wav_end = end
-            cur_str = text
-            reset = False
-        else:
-            wav_end = end
-            cur_str += ' ' + text
-            
-        if wav_end - wav_start >= single_wav_sec:
-            results.append((cur_str, wav_start, wav_end))
-            cur_str = ''
-            reset = True
-    if cur_str != '':
-        results.append((cur_str, wav_start, wav_end))
-    return results
+    if cur_script != '':
+        file_name = str(wav_id).zfill(4)
+        cur_audio.export(f'{output_name}/{file_name}.wav', format="wav")
+        fp = open(f'{output_name}/{file_name}.txt', 'w', encoding='utf-8')
+        print(text, file=fp)
 
 
 
@@ -122,5 +111,4 @@ if __name__ == '__main__':
 
     download_wav(video_url, output_name)
     scripts = get_transcript(output_name, transcript_language)
-    results = split_script(scripts)
-    create_wav_text_pairs(output_name, results)
+    split_wav_script(scripts, output_name)
